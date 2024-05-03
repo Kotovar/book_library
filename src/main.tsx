0b39410d562717fe0e387d22e52a8535b348e3f8
Kotovar <neasit3@gmail.com>
2024-05-03 14:56:12 +0700

    add store, add typed hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-// import { Provider } from "react-redux"
+import { Provider } from "react-redux"
 import App from "./App"
+import store from "./app/store"
 
 const container = document.getElementById("root")
 
@@ -10,9 +11,9 @@ if (container) {
 
   root.render(
     <React.StrictMode>
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 } else {
